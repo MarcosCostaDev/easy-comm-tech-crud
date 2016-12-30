37bc8212d81e81febf4b2475273d40e631115f6d
Marcos Costa <marcos.lcosta@live.com>
2016-12-30 00:39:16 -0300

    adicionando arquivos para navegação e exclusão

---
+++ MvcAngularEntity/Scripts/app.js
@@ -0,0 +1,33 @@
+﻿var CandidadoApp = angular.module("CandidatoApp", ["ngRoute", "CandidatoControllers"]);
+
+
+
+
+CandidadoApp.config([
+    "$routeProvider", function ($routeProvider) {
+
+
+        $routeProvider.when("/listar"
+                , {
+                    templateUrl: "Candidato/Listar.html"
+                    , controller: "ListarController"
+                })
+            .when("/Criar"
+                , {
+                    templateUrl: "Candidato/Editar.html"
+                    , controller: "EditarController"
+                })
+            .when("/Editar/:id"
+                , {
+                    templateUrl: "Candidato/Editar.html"
+                    , controller: "EditarController"
+                })
+            .otherwise(
+                {
+                    redirectTo: "/listar"
+                }
+            );
+
+      
+    }
+]);
---
+++ MvcAngularEntity/Scripts/controller.js
@@ -0,0 +1,56 @@
+﻿var CandidatoControllers = angular.module("CandidatoControllers", []);
+
+CandidatoControllers.controller("ListarController"
+    , ["$scope", "$http"
+    , function($scope, $http) {
+
+
+            var candidatoshttp = $http({
+                method: 'GET'
+                , url: '/api/Candidato'
+                });
+
+        candidatoshttp.then(function(data){
+            $scope.Candidatos = data;
+        }, function(data){
+            
+        });
+
+     
+    }]);
+
+CandidatoControllers.controller("EditarController"
+    , ["$scope", "$filter", "$http", "$routeParams", "$location"
+    , function ($scope, $filter, $http, $routeParams, $location) {
+
+
+        $scope.salvar = function () {
+            if ($scope.candidato.ID) {
+                $http.post("/api/Candidato/", $scope.candidato).success(function (data) {
+                    $location.path("/Listar");
+                }).error(function (data) {
+                    $scope.error = "Um erro ocorreu ao salvar. " + data.ExceptionMessage;
+                });
+            }
+            else {
+                $http.put("/api/Candidato/", $scope.candidato).success(function (data) {
+                    $location.path("/Listar");
+                }).error(function (data) {
+                    $scope.error = "Um erro ocorreu ao salvar. " + data.ExceptionMessage;
+                });
+            }
+        }
+
+        if ($routeParams.Id) {
+            $scope.id = $routeParams.Id;
+            $scope.Titulo = "Editar Candidato";
+
+          $http.get("/api/Candidato")
+          .success(function (data) {
+              $scope.candidato = data;
+          });
+        }
+        else {
+            $scope.Titulo = "Cadastrar um novo candidato";
+        }
+    }]);
